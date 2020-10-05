b9335c3364374c264d6bf7ea0af7d3bbfd00a9a2
Aman Verma <amanverma1729@gmail.com>
2020-10-06 02:09:44 +0530

    Added solutions 9..13

---
+++ exercise13/program.java
@@ -0,0 +1,16 @@
+public class program {
+	public float test(int numbers[]) {
+		/*
+		 * Exercise 13: Average of numbers- Given an array of integers finds the average
+		 * of all the elements. For e.g. for {4,7,9,4} the average is 6 and for
+		 * {1,3,8,5} the average is 4.25. Please note that if the array has no elements,
+		 * the average should be 0.
+		 */
+		if (numbers.length == 0)
+			return 0;
+		int s = 0;
+		for (int i = 0; i < numbers.length; i++)
+			s += numbers[i];
+		return (float) s / (float) numbers.length;
+	}
+}
